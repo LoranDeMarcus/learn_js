801aa031f064fe35d0373c7ef55a13e0fc6a3703
LoranDeMarcus <lorandemarcus@gmail.com>
2017-03-31 20:01:21 +0300

    чистый и изящный код

--- js/script.js
+++ js/script.js
@@ -1,32 +1,22 @@
 var usersData = [];
 
-function AddUser(GetNewUserId, name, email) {
+function AddUser(name, email) {
     usersData.push({
-        id: GetNewUserId,
+        id: GetNewUserId(),
         name: name,
         email: email
-         // здесь id, это максимальный id среди всех
-        // записей пользователей в массиве, т.е функция GetNewUserId() должна вызываться здесь
-        // плюс очередное замечание даже если бы это было правильно - ИМЯ ПЕРЕМЕННО НЕ НЕСЕТ СМЫСЛА
     });
 }
 
-function GetNewUserId(id, userObj) {
+function GetNewUserId(id) {
     if (usersData.id === 0) {
         usersData.id = 1;
     } else {
         var maxId = usersData.reduce((max, item) => item.id > max ? item.id : max, 0);
-        userObj['id'] = maxId + 1;
-        usersData.push(usersData.id);
     }
-    /* TODO:
-     функция для поиска максимального id среди всех пользователей.
-     Возвращает максимальный (id пользователя + 1) из массиве userData, тип Int
-     если в массиве нет пользователей возвращает 1
-     */
+    return maxId + 1;
 }
 
-// данная функция ничего не делает полезного
 function UpdateUser(id, name , email) {
     for (var index = 0; index < usersData.length; index++) {
         if (usersData[index].id === id) {
@@ -49,11 +39,4 @@ function ShowUsers() {
     usersData.forEach(function (item) {
         console.log(item)
     })
-    // todo: сделать вывод в консоле в цикле. что бы отобразилось например:
-    /*
-     User in list:
-     1|Vanya|vanya@mail.ru
-     2|Tema|tema@mail.ru
-     */
-
 }
